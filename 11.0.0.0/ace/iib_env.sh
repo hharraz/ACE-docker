ab4d02d0486c9803a9279eb70c4a1ff515af43d2
HarrazH <h.harraz@gmail.com>
2018-04-04 23:03:51 +0400

    modified:   10.0.0.11/iib/iib_env.sh 	new file:   11.0.0.0/ace/Dockerfile 	new file:   11.0.0.0/ace/iib-license-check.sh 	new file:   11.0.0.0/ace/iib_env.sh 	new file:   11.0.0.0/ace/iib_manage.sh 	new file:   11.0.0.0/ace/kernel_settings.sh

--- 11.0.0.0/ace/iib_env.sh
+++ 11.0.0.0/ace/iib_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2018.
+# © Copyright IBM Corporation 2015.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -8,5 +8,5 @@
 
 if [ -z "$MQSI_VERSION" ]; then
   echo "Sourcing profile"
-  source /opt/ibm/iib-10.0.0.11/server/bin/mqsiprofile
+  source /opt/ibm/ace-11.0.0.0/server/bin/mqsiprofile
 fi
